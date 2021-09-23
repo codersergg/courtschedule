fa197c205967303a4e2ea2911ac48876eacbb5a2
codersergg <sergey.i.chopin@gmail.com>
2021-09-24 00:18:57 +0300

    added new project

---
+++ src/main/java/ru/lawyerworkflow/courtschedule/CourtscheduleApplication.java
@@ -0,0 +1,13 @@
+package ru.lawyerworkflow.courtschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtscheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/lawyerworkflow/courtschedule/CourtscheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lawyerworkflow.courtschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtscheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
